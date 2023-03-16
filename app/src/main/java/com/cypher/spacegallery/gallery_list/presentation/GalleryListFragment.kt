14b3e9e8b04ecf1904061b2a8eb335a6e7fa9d76
cypher <pratikmhatre@gofynd.com>
2023-03-16 12:04:31 +0530

    Added list fragment

---
+++ app/src/main/java/com/cypher/spacegallery/gallery_list/presentation/GalleryListFragment.kt
@@ -0,0 +1,26 @@
+package com.cypher.spacegallery.gallery_list.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cypher.spacegallery.databinding.FragmentGalleryListBinding
+
+class GalleryListFragment : Fragment() {
+    private var _binding: FragmentGalleryListBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGalleryListBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
