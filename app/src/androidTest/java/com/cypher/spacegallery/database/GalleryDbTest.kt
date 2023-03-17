d868607016b0013e8575cdda78ae0b176f81fc63
cypher <pratikmhatre@gofynd.com>
2023-03-17 08:21:44 +0530

    Room db tests

---
+++ app/src/androidTest/java/com/cypher/spacegallery/database/GalleryDbTest.kt
@@ -0,0 +1,88 @@
+package com.cypher.spacegallery.database
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.cypher.spacegallery.core.database.GalleryDb
+import com.cypher.spacegallery.core.database.dao.GalleryItemsDao
+import com.cypher.spacegallery.core.database.entities.GalleryItemTable
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class GalleryDbTest {
+    @get:Rule
+    val instantRule = InstantTaskExecutorRule()
+    private lateinit var galleryDb: GalleryDb
+    private lateinit var galleryItemsDao: GalleryItemsDao
+
+    @Before
+    fun setUp() {
+        galleryDb = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            GalleryDb::class.java
+        ).allowMainThreadQueries().build()
+        galleryItemsDao = galleryDb.galleryItemsDao()
+    }
+
+    @Test
+    fun insertGalleryItemsListTest() = runBlocking {
+        val size = 100
+        val dummyList = getDummyItemList(size)
+        //save
+        galleryItemsDao.insertGalleryItemsList(dummyList)
+
+        //fetch
+        val storedList = galleryItemsDao.getGalleryItemsList()
+
+        assertThat(storedList).hasSize(size)
+    }
+
+    @Test
+    fun deleteAllGalleryItemsTest() = runBlocking {
+        val size = 50
+        val dummyList = getDummyItemList(size)
+
+        //save
+        galleryItemsDao.insertGalleryItemsList(dummyList)
+
+        //delete
+        galleryItemsDao.deleteAllGalleryItems()
+
+        //fetch
+        val storedList = galleryItemsDao.getGalleryItemsList()
+
+        assertThat(storedList).isEmpty()
+    }
+
+
+    @After
+    fun tearDown() {
+        galleryDb.close()
+    }
+
+    private fun getDummyItemList(count: Int): List<GalleryItemTable> {
+        val list = List(count) {
+            GalleryItemTable(
+                id = it.toLong(),
+                date = "Date $it",
+                explanation = "Explanation $it",
+                hdUrl = "Hd Url $it",
+                copyright = null,
+                mediaType = "Type $it",
+                serviceVersion = "Version $it",
+                title = "Title $it",
+                url = "Url $it",
+            )
+        }
+        return list
+    }
+}
