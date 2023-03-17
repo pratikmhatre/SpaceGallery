d1b9b5d8c11d6d0166aadade5d2765f60c5f08dd
cypher <pratikmhatre@gofynd.com>
2023-03-17 11:18:21 +0530

    Back button navigation reverted

--- app/src/main/java/com/cypher/spacegallery/gallery_details/presentation/GalleryDetailsFragment.kt
+++ app/src/main/java/com/cypher/spacegallery/gallery_details/presentation/GalleryDetailsFragment.kt
@@ -55,7 +55,7 @@ class GalleryDetailsFragment : Fragment() {
         })
 
         galleryDetailsAdapter.setOnBackPressListener {
-            findNavController().navigate(R.id.GalleryListFragment)
+            findNavController().navigateUp()
         }
         initObservers()
     }
